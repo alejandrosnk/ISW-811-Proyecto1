d04d9592751da29fa43732bc2a36a5303e45ad00
alejandrosnk <146153840+alejandrosnk@users.noreply.github.com>
2024-06-18 14:07:45 -0600

    Episodio 24.1: se arreglan unos cuantos errores.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,10 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-    protected $guarded=[];
-    // protected $fillable=['title','excerpt','body','id'];
 
-    public function category(){
+    protected $guarded = [];
+
+    public function category()
+    {
         return $this->belongsTo(Category::class);
     }
 }
