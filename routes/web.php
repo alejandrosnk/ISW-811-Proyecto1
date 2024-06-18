c6553aa124740f3fa8cc1e07467b304b292b9171
alejandrosnk <146153840+alejandrosnk@users.noreply.github.com>
2024-06-18 09:20:22 -0600

    Episodio 17:se crea la base de datos y se muestran las propiedades de la base de datos

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,8 @@ Route::get('/', function () {
 });
 
 Route::get('posts/{post}', function ($slug) {
-    
     return view ('post', [
-        $post=Post::findOrFail($slug);
+        'post' => Post::findOrFail($slug)
     ]);
 });
 // })->where('post', '[A-z_\-]+');
