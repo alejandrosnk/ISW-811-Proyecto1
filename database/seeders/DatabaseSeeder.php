a54fa4745840678b62eb1932f8c03541c369f568
alejandrosnk <146153840+alejandrosnk@users.noreply.github.com>
2024-06-23 01:54:53 -0600

    Episodio 27: se agregan los autores a los posts, se hacen clickeables aunque sin funcionalidad y se almacenan de forma optima los posts en la base de datos.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\User;
+use App\Models\Post;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +17,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Post::truncate();
+        Category::truncate();
+
+        $user=User::factory()->create();
+
+        $personal=Category::create([
+            'name'=>'Personal',
+            'slug'=>'personal'
+        ]);
+        $family=Category::create([
+            'name'=>'Family',
+            'slug'=>'family'
+        ]);
+        $work=Category::create([
+            'name'=>'Work',
+            'slug'=>'work'
+        ]);
+        Post::create([
+            'user_id'=>$user->id,
+            'category_id'=>$family->id,
+            'title'=>'My Family post',
+            'slug'=>'my-first-post',
+            'excerpt'=>'<p>Lorem ipsum dolor sit amet.</p>',
+            'body'=>'<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda qui amet consectetur obcaecati. Laudantium sequi facere illo asperiores dolorem aut reprehenderit vitae repudiandae soluta. Libero nostrum ipsa facere aliquam iste.</p>'
+        ]);
+
+        Post::create([
+            'user_id'=>$user->id,
+            'category_id'=>$work->id,
+            'title'=>'My Work post',
+            'slug'=>'my-work-post',
+            'excerpt'=>'<p>Lorem ipsum dolor sit amet.</p>',
+            'body'=>'<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda qui amet consectetur obcaecati. Laudantium sequi facere illo asperiores dolorem aut reprehenderit vitae repudiandae soluta. Libero nostrum ipsa facere aliquam iste.</p>'
+        ]);
     }
 }
