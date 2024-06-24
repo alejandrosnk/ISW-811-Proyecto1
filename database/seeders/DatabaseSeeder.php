1e35f1aaa14a50cbee469f26d80d7bb1d7d4a128
alejandrosnk <146153840+alejandrosnk@users.noreply.github.com>
2024-06-24 06:37:29 -0600

    Episodio 28: se agregan las funciones necesarioas parq que los posts(categorias y usurios y el contenido de cada posts sea agregado dinamicamente en la db.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,37 +20,12 @@ class DatabaseSeeder extends Seeder
         User::truncate();
         Post::truncate();
         Category::truncate();
-
-        $user=User::factory()->create();
-
-        $personal=Category::create([
-            'name'=>'Personal',
-            'slug'=>'personal'
+        
+        $user=User::factory()->create([
+            'name'=>'Jon Doe'
         ]);
-        $family=Category::create([
-            'name'=>'Family',
-            'slug'=>'family'
-        ]);
-        $work=Category::create([
-            'name'=>'Work',
-            'slug'=>'work'
-        ]);
-        Post::create([
-            'user_id'=>$user->id,
-            'category_id'=>$family->id,
-            'title'=>'My Family post',
-            'slug'=>'my-first-post',
-            'excerpt'=>'<p>Lorem ipsum dolor sit amet.</p>',
-            'body'=>'<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda qui amet consectetur obcaecati. Laudantium sequi facere illo asperiores dolorem aut reprehenderit vitae repudiandae soluta. Libero nostrum ipsa facere aliquam iste.</p>'
-        ]);
-
-        Post::create([
-            'user_id'=>$user->id,
-            'category_id'=>$work->id,
-            'title'=>'My Work post',
-            'slug'=>'my-work-post',
-            'excerpt'=>'<p>Lorem ipsum dolor sit amet.</p>',
-            'body'=>'<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda qui amet consectetur obcaecati. Laudantium sequi facere illo asperiores dolorem aut reprehenderit vitae repudiandae soluta. Libero nostrum ipsa facere aliquam iste.</p>'
+        Post::factory(5)->create([
+            'user_id'=>$user->id
         ]);
     }
 }
