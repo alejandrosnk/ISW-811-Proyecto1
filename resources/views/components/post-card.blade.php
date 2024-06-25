5ae7dc8bf98db3dc6d6c5012551849a8c8c1f1e8
alejandrosnk <146153840+alejandrosnk@users.noreply.github.com>
2024-06-25 09:30:07 -0600

    Episodio 36: limpieza de código y arreglos de mejora para el formato de los posts

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -30,9 +30,7 @@
 
                             <div class="text-sm mt-4">
                                 <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                {!! $post->excerpt !!}
                                 </p>
 
                                 <p class="mt-4">
