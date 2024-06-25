a69fb525e97e5866d484706673955b97028a0b9e
alejandrosnk <146153840+alejandrosnk@users.noreply.github.com>
2024-06-25 08:44:50 -0600

    Episodio 34: se añade la función de dropdown de las categorias y se hacen funcionales, además usamos alpine

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,10 @@ use App\Models\Category;
 */
 
 Route::get('/', function () {
-    // \Illuminate\Support\Facades\DB::listen(function($query){
-    //     logger($query->sql,$query->bindings);
-    // });
     return view('posts', [
-        'posts' => Post::latest()->get()
+        'posts' => Post::latest()->get(),
+        'categories' => Category::all(),
+        'currentCategory' => $category ?? null
     ]);
 });
 
@@ -34,13 +33,16 @@ Route::get('posts/{post:slug}', function (Post $post) {
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'categories' => Category::all(),
+        'currentCategory' => $category ?? null
     ]);
 });
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts,
+        'categories' => Category::all()
     ]);
 });
 
