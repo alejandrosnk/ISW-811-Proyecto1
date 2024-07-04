68656bc0c62301c3f00d16c8c1c688e18d22b371
alejandrosnk <146153840+alejandrosnk@users.noreply.github.com>
2024-07-04 13:35:10 -0600

    Episodio 60: separamos en componentes las funcionalidades de el Newsletter en cdiferentes componentes y seguros al usar variables de entorno.

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Models\User;
 use App\Models\Post;
 use App\Models\Category;
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\SessionsController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\PostCommentsController;
@@ -18,43 +19,17 @@ use App\Http\Controllers\RegisterController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::post('newsletter', function () {
-    request()->validate(['email' => 'required|email']);
-    
-    $mailchimp = new \MailchimpMarketing\ApiClient();
-    
-    $mailchimp->setConfig([
-        'apiKey' => config('services.mailchimp.key'),
-        'server' => 'us18'
-    ]);
-    try{
-        $response = $mailchimp->lists->addListMember('9ff5300f37', [
-            'email_address' => request('email'),
-            'status' => 'subscribed'
-        ]);
-    }catch(\Exception $e){
-        throw \Illuminate\Validation\ValidationException::withMessages([
-            'email'=>'This email could not bu added to our newsletter list.'
-        ]);
-    }
-    
-    
-    return redirect('/')->with('success', 'You are now signed up for our newsletter!');
-});
-
-
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
-
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
+
+Route::post('newsletter', NewsletterController::class);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
-
 Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
 Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
-
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
 
 //Auth::routes();
