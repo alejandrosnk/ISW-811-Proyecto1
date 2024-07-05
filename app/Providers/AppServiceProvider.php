4883791bd6f8d1d28c5586f88b452df45a590b64
alejandrosnk <146153840+alejandrosnk@users.noreply.github.com>
2024-07-05 15:01:51 -0600

    Episodio 69: se restringe el acceso a las barras de navegación paera solo los usuarios administradores

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use App\Services\Newsletter;
 use App\Services\MailchimpNewsletter;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use MailchimpMarketing\ApiClient;
 
@@ -20,13 +23,11 @@ class AppServiceProvider extends ServiceProvider
         app()->bind(Newsletter::class, function () {
             $client = (new ApiClient)->setConfig([
                 'apiKey' => config('services.mailchimp.key'),
-                'server' => 'us18'
+                'server' => 'us6'
             ]);
-
             return new MailchimpNewsletter($client);
         });
     }
-
     /**
      * Bootstrap any application services.
      *
@@ -35,5 +36,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Model::unguard();
+
+        Gate::define('admin', function (User $user) {
+            return $user->username === 'tyrion';
+        });
+
+        Blade::if('admin', function () {
+            return request()->user()?->can('admin');
+        });
     }
 }
